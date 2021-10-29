2ceea4b29007598617f07fc5900509e5f8a84f36
yuebing <jie5241666@163.com>
2021-10-29 16:24:15 +0800

    update

---
+++ config.sh
@@ -0,0 +1,27 @@
+config(){
+	echo "Input hostname"
+	read TMP
+	echo TMP > /etc/hostname
+	while :
+	do
+		echo "Input root password"
+		read pwd1
+		echo "Input root password again"
+		read pwd2
+		if [ $pwd1 != $pw2 ];then
+			echo "Two input password must must be consistent"
+		else
+			break
+		fi
+	done
+	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	hwclock --systohc
+	echo -e "en_US.UTF-8\nzh_CN.UTF-8" > /etc/locale.conf
+	locale-gen
+	echo "LANG=en_US.UTF-8" > /etc/locale.conf
+	echo -e "127.0.0.1\tlocalehost\n::1\tlocalehost\n127.0.1.1\t$TMP.localdomain\t$TMP" >> /etc/hosts
+}
+main(){
+	config
+}
+main
--- install.sh
+++ install.sh
@@ -10,35 +10,16 @@ partion_format(){
 	mkfs.ext4 ${TMP}2 1>/dev/null 1>&2
 	echo "FAT32 EXT4 done"
 }
-install_config(){
+install(){
 	mount /dev/sda2 /mnt
 #	mkdir /mnt/boot
 	mount /dev/sda1 /mnt/boot
 #	pacstrap /mnt base linux linux-firmware	--force
 #	pacman -Syy
     genfstab -U -p /mnt >> /mnt/etc/fstab
-	arch-chroot /mnt
-	echo "Input hostname"
-	read TMP
-	echo TMP > /etc/hostname
-	while :
-	do
-		echo "Input root password"
-		read pwd1
-		echo "Input root password again"
-		read pwd2
-		if [ $pwd1 != $pw2 ];then
-			echo "Two input password must must be consistent"
-		else
-			break
-		fi
-	done
-	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-	hwclock --systohc
-	echo -e "en_US.UTF-8\nzh_CN.UTF-8" > /etc/locale.conf
-	locale-gen
-	echo "LANG=en_US.UTF-8" > /etc/locale.conf
-	echo -e "127.0.0.1\tlocalehost\n::1\tlocalehost\n127.0.1.1\t$TMP.localdomain\t$TMP" >> /etc/hosts
+	cp config.sh /mnt/root/
+	chmod +x config.sh
+	arch-chroot /mnt /root/config.sh
 }
 main(){
 	install_config
